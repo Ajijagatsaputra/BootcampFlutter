ef1d2a3efef69f6a31d30933c31da1e35ebdac66
Ajijagatsaputra <ajijagat90@gmail.com>
2024-12-10 08:28:09 +0700

    Menambahkan Tugas-6

---
+++ Tugas-6-AsyncAwaitFuture/soal2.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  Future.delayed(Duration(seconds: 1), () {
+    print('Life');
+  });
+  Future.delayed(Duration(seconds: 3), () {
+    print('never flat');
+  });
+  Future.delayed(Duration(seconds: 2), () {
+    print('is');
+  });
+}
